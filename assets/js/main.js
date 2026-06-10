cd404e1fe537de067108fa74021e69f25766b316
Simon Meggle <simon.meggle@elabit.de>
2026-06-10 14:43:57 +0200

    Testimonials

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,13 @@
-import { init as initNav }          from './nav.js';
-import { init as initGapViz }       from './gap-viz.js';
-import { init as initNewsletter }   from './newsletter.js';
-import { init as initLeadMagnets }  from './lead-magnets.js';
+import { init as initNav }           from './nav.js';
+import { init as initGapViz }        from './gap-viz.js';
+import { init as initNewsletter }    from './newsletter.js';
+import { init as initLeadMagnets }   from './lead-magnets.js';
+import { init as initTestimonials }  from './testimonials.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initNav();
   initGapViz();
   initNewsletter();
   initLeadMagnets();
+  initTestimonials();
 });
